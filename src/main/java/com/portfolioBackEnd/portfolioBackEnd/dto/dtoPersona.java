c02818229c451d302eb6b267dd73a2c7bf4bf9d6
David <davidginaca@gmail.com>
2023-01-06 08:33:43 -0300

    preparando para subir

---
+++ src/main/java/com/portfolioBackEnd/portfolioBackEnd/dto/dtoPersona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolioBackEnd.portfolioBackEnd.dto;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author david
+ */
+@Getter @Setter
+public class dtoPersona {
+
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String apellido;
+    @NotBlank
+    private String descripcion;
+
+    private String img;
+
+    public dtoPersona() {
+    }
+
+    public dtoPersona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+    
+    
+    
+}
--- src/main/java/com/portfolioBackEnd/portfolioBackEnd/model/persona.java
+++ src/main/java/com/portfolioBackEnd/portfolioBackEnd/model/persona.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
@@ -30,8 +30,20 @@ public class Persona implements Serializable {
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @NotNull
+    private String descripcion;
+    
     private String img;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String descripcion, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
 
        
     
