053871730623bb211e2f46bf108d7ca13550fee0
David <davidginaca@gmail.com>
2023-01-10 08:10:17 -0300

    .

--- src/main/java/com/portfolioBackEnd/portfolioBackEnd/Security/Controller/AuthController.java
+++ src/main/java/com/portfolioBackEnd/portfolioBackEnd/Security/Controller/AuthController.java
@@ -38,7 +38,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin()
+@CrossOrigin("https://front-portfolio-7cfaf.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
